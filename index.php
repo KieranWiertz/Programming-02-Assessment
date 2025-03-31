9f4a23315b1da02993697d266d923e4dca75633f
KieranWiertz <kieranwiertz@icloud.com>
2025-03-31 14:42:44 +0100

    Footer Added

--- index.php
+++ index.php
@@ -40,5 +40,37 @@
         </div>
       </div>
     </section>
+
+    <footer>
+      <div class="footer-container">
+        <div class="footer-heading footer-1">
+          <h2>Categories</h2>
+          <a href="#">About Us</a>
+          <a href="vinyl-page.php">Vinyl</a>
+          <a href="cd-page.php">CD</a>
+          <a href="login-page.php">Login</a>
+          <a href="signup-page.php">Sign Up</a>
+       </div>
+       <div class="footer-heading footer-2">
+          <h2>Contact Us</h2>
+          <h3>Email:<br> RecordStore@Contact.co.uk</h3>
+          <h3>Phone:<br> 07 123 123 123</h3>
+          <h3>Address:<br> 123 Test Street<br>Testville<br>Testland</h3>
+       </div>
+       <div class="footer-heading footer-3">
+          <h2>Social Media</h2>
+          <a href="#">Instagram</a>
+          <a href="#">Facebook</a>
+          <a href="#">YouTube</a>
+          <a href="#">Twitter</a>
+       </div>
+       <div class="footer-newsletter">
+        <h2>Join Our Newsletter</h2>
+        <input type= "email" placeholder="Enter Your Email Address" id="footer-email">
+        <input type="submit" value="Join" id="footer-newsletter-btn">
+       </div>
+      </div>
+    </footer>
+
   </body>
 </html>
