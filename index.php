e9a36138c8722a04f540c45457913c156470453c
KieranWiertz <kieranwiertz@icloud.com>
2025-03-31 20:09:02 +0100

    Footer Changes

--- index.php
+++ index.php
@@ -109,18 +109,17 @@
     <footer>
       <div class="footer-container">
         <div class="footer-heading footer-1">
+          <h2>Contact Us</h2>
+          <h3>Email:<br> Contact@RecordStore.co.uk</h3>
+          <h3>Phone:<br> 07 123 123 123</h3>
+          <h3>Address:<br> 123 Test Street<br>Testville<br>Testland</h3>
+       </div>
+       <div class="footer-heading footer-2">
           <h2>Categories</h2>
-          <a href="#">About Us</a>
           <a href="vinyl-page.php">Vinyl</a>
           <a href="cd-page.php">CD</a>
           <a href="login-page.php">Login</a>
           <a href="signup-page.php">Sign Up</a>
-       </div>
-       <div class="footer-heading footer-2">
-          <h2>Contact Us</h2>
-          <h3>Email:<br> RecordStore@Contact.co.uk</h3>
-          <h3>Phone:<br> 07 123 123 123</h3>
-          <h3>Address:<br> 123 Test Street<br>Testville<br>Testland</h3>
        </div>
        <div class="footer-heading footer-3">
           <h2>Social Media</h2>
