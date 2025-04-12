ab418d8b91d775b244409dccc98462a1d0c709aa
KieranWiertz <kieranwiertz@icloud.com>
2025-04-12 15:37:18 +0100

    bug fixes

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 function emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) {
-    $result;
+    $result = null;
     if (empty($name) || empty($email) || empty($username) || empty($pwd) || empty($pwdRepeat)) {
         $result = true;
     }
@@ -12,7 +12,7 @@ function emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) {
 }
 
 function invalidUid($username) {
-    $result;
+    $result = null;
     if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
         $result = true;
     }
@@ -23,7 +23,7 @@ function invalidUid($username) {
 }
 
 function invalidEmail($email) {
-    $result;
+    $result = null;
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $result = true;
     }
@@ -34,7 +34,7 @@ function invalidEmail($email) {
 }
 
 function pwdMatch($pwd, $pwdRepeat) {
-    $result;
+    $result = null;
     if ($pwd !== $pwdRepeat) {
         $result = true;
     }
@@ -86,7 +86,7 @@ function createUser($conn, $name, $email, $username, $pwd) {
 }
 
 function emptyInputLogin($username, $pwd) {
-    $result;
+    $result = null;
     if (empty($username) || empty($pwd)) {
         $result = true;
     }
