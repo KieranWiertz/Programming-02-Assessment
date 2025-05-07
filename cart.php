8ea33d8f7c749383a29e4faf7a98b1257a4eecdb
KieranWiertz <kieranwiertz@icloud.com>
2025-05-07 12:34:29 +0100

    cart styling added

--- cart.php
+++ cart.php
@@ -1,7 +1,16 @@
 <?php include_once 'header.php'; ?>
 
-<h1>[username]'s cart</h1>
+    <div class="cart-user-info">
+        <?php
+          if (isset($_SESSION["useruid"])) {
+            echo "<p>" . $_SESSION["useruid"] . "'s Shopping Cart </p>";
+          }
+    ?>
+    </div>
+    <h2 class="cart-info">We hope you where able to find whatever you where looking for!<br>Check out your items here</h2>
 
-<h3>[checkout system]</h3>
+    <div class="cart-items">
+        
+    </div>
 
 <?php include_once 'footer.php'; ?>
