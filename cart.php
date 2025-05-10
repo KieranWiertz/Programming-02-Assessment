8d10c7cf1b77657665f542777c102c650ef28db5
KieranWiertz <kieranwiertz@icloud.com>
2025-05-10 20:13:00 +0100

    cart page styling

--- cart.php
+++ cart.php
@@ -1,3 +1,15 @@
 <?php include_once 'header.php'; ?>
 
+    <div class="cart-user-info">
+        <?php
+          if (isset($_SESSION["useruid"])) {
+            echo "<p>" . $_SESSION["useruid"] . "'s Cart</p>";
+          }
+        ?>
+    </div>
+    <h2 class="video-description">We hope you are able to find everything<br>Check out when ready!</h2>
+
+    <div class="checkout-system">
+
+    </div>
 <?php include_once 'footer.php'; ?>
